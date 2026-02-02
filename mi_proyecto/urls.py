8acbfd53cff43ee017f781eb51640a5ec30f2cd9
elkin1123 <elkinjoshuadelgadolesp68@gmail.com>
2026-02-01 19:01:32 -0500

    Fix: Corrección de nombres de proyecto (cv_elkin -> mi_proyecto)

--- mi_proyecto/urls.py
+++ mi_proyecto/urls.py
@@ -21,9 +21,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tasks.urls')),  # Incluye las URLs de tu app 'tasks'
+    path('', include('tasks.urls')),
 ]
 
-# Servir archivos multimedia en desarrollo
+# Solo en desarrollo
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
